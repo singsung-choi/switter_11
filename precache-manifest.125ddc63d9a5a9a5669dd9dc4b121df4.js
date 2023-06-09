6984e27368596fa5e8592e277ff632dfe06bff18
singsung-choi <xkseh5424@gmail.com>
2023-06-10 00:52:45 +0900

    Updates

---
+++ precache-manifest.125ddc63d9a5a9a5669dd9dc4b121df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727f1d74df79126a557e3986816a19a3",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "1378c498f5142ee40128",
+    "url": "/nwitter/static/css/main.da5830ff.chunk.css"
+  },
+  {
+    "revision": "aaa6dd6e1535bc75a2b9",
+    "url": "/nwitter/static/js/2.ca9f81f2.chunk.js"
+  },
+  {
+    "revision": "34981cc0942697370fc9d635062a9d94",
+    "url": "/nwitter/static/js/2.ca9f81f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1378c498f5142ee40128",
+    "url": "/nwitter/static/js/main.6a913699.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
